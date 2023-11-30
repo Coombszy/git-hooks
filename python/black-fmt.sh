8f77b2c90ef7815e395d8c9e1322bdc157a5bde6
Liam Coombs <LCCoombs@hotmail.co.uk>
2023-11-30 14:56:17 +0000

    feat: added python support
    Added black-fmt for python. Also removed install checks from any other
    scripts. Added pipefail to bash scripts.

---
+++ python/black-fmt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -euo pipefail
+
+python -m black --no-color --diff --check .
+
--- terraform/tflint.sh
+++ terraform/tflint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
-# Is tflint installed?
-if ! [ -x "$(command -v tflint)" ]; then
-  echo 'Error: tflint is not installed.' >&2
-  exit 1
-fi
-
-# Run tflint
 tflint --no-color --recursive
 
