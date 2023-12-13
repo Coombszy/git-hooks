09e4d5199b2159e018fd5624647842173246750c
Liam Coombs <LCCoombs@hotmail.co.uk>
2023-12-13 11:04:40 +0000

    feat: improved config error/exception handling (#7)

--- hooks/lib/config.py
+++ hooks/lib/config.py
@@ -1,3 +1,4 @@
+# Copyright (C) 2023  Coombszy
 # This lib handles loading config from config.json. If config.json is not
 # found, it will create one with default values.
 #
@@ -10,7 +11,11 @@ import os
 class Config:
     def __get_hooks_path(self):
         """Get hooks path from Git global config"""
-        return os.popen("git config --global core.hooksPath").read().strip()
+        try:
+            # Get hooks path from Git global config
+            return os.popen("git config --global core.hooksPath").read().strip()
+        except Exception as e:
+            raise RuntimeError("Failed to get hooks path from Git. Is it installed?") from e
 
     def __get_config_path(self):
         """Get config path using relative path from hooks path"""
@@ -44,7 +49,7 @@ class Config:
                 print("OK!")
             except Exception as e:
                 print("FAILED!")
-                raise Exception("Failed to create config.") from e
+                raise OSError("Failed to create config.") from e
 
         # Load config
         self.__load_config()
@@ -54,10 +59,13 @@ class Config:
         # Get config path
         config_path = self.__get_config_path()
 
-        # Open config file (UTF-8)
-        with open(config_path, "r", encoding="utf-8") as config_file:
-            # Load config
-            self.__config = json.load(config_file)
+        try :
+            # Open config file (UTF-8)
+            with open(config_path, "r", encoding="utf-8") as config_file:
+                # Load config
+                self.__config = json.load(config_file)
+        except Exception as e:
+            raise OSError("Failed to load config.") from e
 
     def get_config(self):
         """Get config"""
