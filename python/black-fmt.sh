d4c51cab94ee11779c525f7fbc3aab749e69e580
Liam Coombs <LCCoombs@hotmail.co.uk>
2023-12-13 10:51:14 +0000

    chore: added GNU GPL License (#6)

--- python/black-fmt.sh
+++ python/black-fmt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Copyright (C) 2023  Coombszy
 set -euo pipefail
 
 python -m black --no-color --diff --check --quiet .
--- terraform/validate.sh
+++ terraform/validate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Copyright (C) 2023  Coombszy
 set -euo pipefail
 
 terraform validate -no-color
