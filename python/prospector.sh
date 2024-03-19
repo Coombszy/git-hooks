5e7e2dd64c8aba03efa5039ecf96017e9242eeb3
Liam Coombs <LCCoombs@hotmail.co.uk>
2024-03-19 13:53:06 +0000

    fix(python): skip .terraform directories

--- python/prospector.sh
+++ python/prospector.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 # Prospector is really slow for some reason scanning a repo with a lot of files.
 # So use find and xargs to run it.
-find . -name "*.py" | xargs python -m prospector -M
+find . -not \( -path *.terraform -prune \) -name "*.py" | xargs python -m prospector -M
 
